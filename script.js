5a87df82f37ee1da25d414ebb796262d274a34e1
Ben Cullen <84505694+BenCullenDev@users.noreply.github.com>
2022-04-25 12:00:10 +0100

    seeing if https works

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ let span = document.getElementById("location");
 button.addEventListener("click", handleClick)
 
 async function handleClick() {
-    const response = await fetch('http://api.open-notify.org/iss-now.json');
+    const response = await fetch('https://api.open-notify.org/iss-now.json');
     const location = await response.json();
     span.innerHTML = `The ISS is currently located at latitude: ${location.iss_position.latitude} longitude: ${location.iss_position.longitude}`;
 
